ff7d2319c2c4a32dadc85f0ee1c397f1773abc72
Kadvin <kadvin@gmail.com>
2015-02-03 14:38:12 +0800

    Support util properties to be read by other package

--- utils/src/main/java/net/happyonroad/UtilAppConfig.java
+++ utils/src/main/java/net/happyonroad/UtilAppConfig.java
@@ -5,6 +5,9 @@ package net.happyonroad;
 
 import net.happyonroad.config.ApplyToSystemProperties;
 import net.happyonroad.spring.config.AbstractAppConfig;
+import org.springframework.beans.factory.config.PropertyResourceConfigurer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -18,7 +21,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 // context:property-placeholder 里面的 location 包括通配符的功能
 //  通过 @PropertySource 无法实现
 @ImportResource("classpath:META-INF/properties.xml")
-public class UtilAppConfig extends AbstractAppConfig{
+public class UtilAppConfig extends AbstractAppConfig {
 
     @Bean
     ApplyToSystemProperties applyToSystemProperties(){
@@ -37,5 +40,14 @@ public class UtilAppConfig extends AbstractAppConfig{
     @Override
     public void doExports()  {
         exports(TaskScheduler.class, "system");
+    }
+
+    @Override
+    protected void afterExports() {
+        //把 本模块的 PropertyResourceConfigurer 注册到 parent context中
+        // 以便其他模块在使用 ContextUtils.inheritParentProperties 时能够找到一个可用的
+        PropertyResourceConfigurer configurer = applicationContext.getBean(PropertyResourceConfigurer.class);
+        ApplicationContext parent = applicationContext.getParent();
+        ((ConfigurableApplicationContext)parent).addBeanFactoryPostProcessor(configurer);
     }
 }
