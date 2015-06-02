5d6b1aac3084e4451a77d3f392ef368fca73f91e
Kadvin <kadvin@gmail.com>
2015-06-02 09:28:40 +0800

    Add highest method into config/perform status

--- basic/src/main/java/net/happyonroad/type/ConfigStatus.java
+++ basic/src/main/java/net/happyonroad/type/ConfigStatus.java
@@ -3,14 +3,22 @@
  */
 package net.happyonroad.type;
 
+import java.util.Collections;
+import java.util.Set;
+
 /**
  * <h1>配置状态</h1>
  */
 public enum ConfigStatus {
-    // 配置未变更
-    Changed,
-    // 配置发生变更
-    Unchanged,
     // 配置变化与否未知
-    Unknown
+    Unknown, //ordinal = 0
+    // 配置发生变更
+    Unchanged,//ordinal = 1
+    // 配置未变更
+    Changed; //ordinal = 2
+
+
+    public static ConfigStatus highest(Set<ConfigStatus> configStatuses) {
+        return Collections.max(configStatuses);
+    }
 }
--- basic/src/main/java/net/happyonroad/type/Performance.java
+++ basic/src/main/java/net/happyonroad/type/Performance.java
@@ -3,9 +3,12 @@
  */
 package net.happyonroad.type;
 
+import java.util.Collections;
+import java.util.Set;
+
 /**
  * <h1>性能状态</h1>
- *
+ * <p/>
  * 这可能是性能指标的状态，也可能是组件或者资源/链路的性能状态
  * <ul>
  * 有以下几个注意点:
@@ -14,13 +17,31 @@ package net.happyonroad.type;
  * </ul>
  * 指标状态的原始来源：可计算的性能指标，如CPU平均利用率，落在不同的区间，导致不通的指标状态
  */
-public enum Performance {
-    // Normal: 正常工作中，对应绿色
+public enum Performance implements Comparable<Performance>{
+    // Unknown, 性能状态未知, ordinal = 0
+    Unknown,
+    // Normal: 正常工作中，对应绿色，ordinal = 1
     Normal,
-    // Warning: 警告状态，对应黄色，或者叫做 “轻微超标”
+    // Warning: 警告状态，对应黄色，或者叫做 “轻微超标”，ordinal = 2
     Warning,// Minor in Mocha
-    // Error：错误状态，对应红色，或者叫做 “严重超标”
-    Critical,  // Serious in Mocha
-    // 未知
-    Unknown
+    // Error：错误状态，对应红色，或者叫做 “严重超标”， ordinal = 3
+    Critical;  // Serious in Mocha
+
+    /**
+     * <h2>下一级性能状态</h2>
+     *
+     * @return 性能状态
+     */
+    public Performance decrease() {
+        if (this == Critical)
+            return Warning;
+        else if (this == Warning)
+            return Normal;
+        else
+            return this;
+    }
+
+    public static Performance highest(Set<Performance> set) {
+        return Collections.max(set);
+    }
 }
