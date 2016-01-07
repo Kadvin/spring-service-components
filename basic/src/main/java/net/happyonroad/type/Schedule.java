5636ed2253e4e38799f7335ecb73745311797ffa
Kadvin <kadvin@gmail.com>
2016-01-07 16:32:44 +0800

    Refactor the schedule, it's not subclass of TimeSpan

--- basic/src/main/java/net/happyonroad/type/Schedule.java
+++ basic/src/main/java/net/happyonroad/type/Schedule.java
@@ -3,26 +3,72 @@
  */
 package net.happyonroad.type;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.io.Serializable;
+
 /**
  * 监控计划
- * TODO: enhance it to support intervals, matrix
  */
-public class Schedule extends TimeSpan {
+public class Schedule implements Serializable {
     private static final long serialVersionUID = 4754218267494981545L;
 
-    private String cron;
+    // 调度类型，分为: Cron, Intervals, Matrix 三种
+    private ScheduleType type = ScheduleType.Cron;
 
-    public String getCron() {
-        return cron;
+    // CRON表达式
+    private String   expression;
+    // 时间段
+    private String[] intervals;
+    // 时间表
+    private String[] matrix;
+
+    public ScheduleType getType() {
+        return type;
     }
 
-    public void setCron(String cron) {
-        this.cron = cron;
+    public void setType(ScheduleType type) {
+        this.type = type;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public String[] getIntervals() {
+        return intervals;
+    }
+
+    public void setIntervals(String[] intervals) {
+        this.intervals = intervals;
+    }
+
+    public String[] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(String[] matrix) {
+        this.matrix = matrix;
     }
 
     @Override
     public String toString() {
-        return "Schedule(" + format(getStartAt(), "ever") + " -> " + format(getEndAt(), "forever") + ":" + cron + ")";
+        return "Schedule(" + type + "=" + value() + ")";
+    }
+
+    public String value() {
+        switch (type) {
+            case Cron:
+                return expression;
+            case Matrix:
+                return StringUtils.join(matrix, ",");
+            default:
+                return StringUtils.join(intervals, ",");
+        }
     }
 
 }
---
+++ basic/src/main/java/net/happyonroad/type/ScheduleType.java
@@ -0,0 +1,10 @@
+package net.happyonroad.type;
+
+/**
+ * <h1>调度类型</h1>
+ *
+ * @author Jay Xiong
+ */
+public enum ScheduleType {
+    Intervals, Matrix, Cron
+}
